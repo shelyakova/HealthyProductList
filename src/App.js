4ebb150b28fe7587b3001023bacb548d8f37fd8f
shelyakova <s.sheliakova@gmail.com>
2018-07-21 01:08:15 +0300

    add correct list. Change styles at first. Add pretty checkboxes.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,91 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './images/logo.png';
+import { connect } from "react-redux";
+import Checkbox from 'rc-checkbox';
+import 'rc-checkbox/assets/index.css';
 
 class App extends Component {
+  state = {
+    editId: null,
+    editValue: null,
+    editDate: null,
+    disabled: false
+  };
+
   render() {
+    const { data,
+      handleClick = () => {},
+      onDelete = () => {},
+      onEdit = () => {},
+      onClick = () => {
+        this.setState
+      }
+    } = this.props;
     return (
       <div className="App">
         <div className="appHeader">
-        <img src={logo} className="appLogo" />
+          <img src={logo} className="appLogo" />
+        </div>
+      <div className="container">
+        <div className="contentAlign">
+          <div className="addProductInput">
+            <input name="name" ref={el => (this.input = el)} />
+            <button
+              onClick={() => {
+                this.input.value && handleClick(this.input.value);
+                this.input.value = "";
+              }}
+              >
+              Add
+            </button>
+          </div>
+          <div className="productList">
+            {data.map((i, key) => (
+              <div className="listItem" key={key}>
+                <label>
+                  <Checkbox
+                    type="checkbox"
+                    disabled={this.state.disabled}
+                  />
+                    {i.value}
+                </label>
+                <button className="buttonDelete" onClick={() => {
+                  if(this.state.editId !== i.date) {
+                    onDelete(key);
+                    this.setState({editId: null});
+                  }
+                }}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
+    </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    data: state
+  }),
+  dispatch => ({
+    handleClick: value =>
+      dispatch({
+        type: "ADD_TRACK",
+        payload: {
+          date: new Date().valueOf(),
+          value: value
+        }
+      }),
+    onDelete: idx =>
+      dispatch({
+        type: "DELETE_TRACK",
+        payload: idx
+      })
+  })
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from "redux";
+
+function playlist(state = [], action) {
+
+  if (action.type === "ADD_TRACK") {
+    return [...state, action.payload];
+  }
+  if (action.type === "DELETE_TRACK") {
+    state.splice(action.payload, 1);
+    return [...state];
+  }
+
+  return [...state];
+}
+
+const store = createStore(playlist);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
